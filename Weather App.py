92efec3113998cf7eec7f4d6c4963db5352b81a0
이진영 <jakerhee321@gmail.com>
2023-02-16 12:58:54 +0900

    Basic Frame

--- Weather App.py
+++ Weather App.py
@@ -1,1 +1,12 @@
 import PySimpleGUI as sg
+
+layout = [[]]
+
+window = sg.Window('Weather', layout)
+
+while True:
+    event, values = window.read()
+    if event == sg.WIN_CLOSED:
+        break
+
+window.close()
